35dd0d86ef081aa69eb622a4cebfcd430e0cd334
Boubacar Telly Bah <boubacar-telly.bah@polymtl.ca>
2017-10-20 22:17:02 -0400

    Ajout de commentaire et entete de fonction

--- TP1/src/Hasse.py
+++ TP1/src/Hasse.py
@@ -6,26 +6,49 @@ class Hasse:
         pass
     
     def is_in_relation(self, connections, node1, node2):
-          for connectedNode in connections[node1]:
-                if connectedNode == node2:
-                    return True
-          return False
+        """
+        This function find if node 1 and node 2 are connected
+        parameters :
+            connections -- Hashmap that contains the node as key and his 
+            connection as value
+            
+            node1,Node2 -- the two nodes which we will determine if they
+            are connected
+        """
+        for connected_node in connections[node1]:
+            if connected_node == node2:
+                return True
+        return False
       
     def remove_relexivity(self, connections):
+        """
+        This function delete the reflexivity in a graph
+            connections -- Hashmap that contains the node as key and his 
+            connection as value
+            
+        """
         for node in connections:
             if self.is_in_relation(connections,node,node):
+                #If a node is connected to himself then remove it
                 connections[node].remove(node)
                     
         
     def remove_transitivity(self, connections):
+        """
+        This function delete transitivity
+        parameters :
+            connections -- Hashmap that contains the node as key and his 
+            connection as value
+            
+        """
         for node in connections:
-            #print(connections[node])
-            commonNodes = connections[node]
-            for connectedNode in connections[node] : 
-                connection = self.get_connected_node(connections,connectedNode)
-                commonNodes = list(set(commonNodes).intersection(connection)) #prendre les elements en commun
-                connections[node] = list(set(connections[node])- set(commonNodes))
-            print("connection",connections[node])
+            common_nodes = connections[node]
+            for connected_node in connections[node] : 
+                #For all the neighbour of our current node remove the common elements
+                connection = connections[connected_node]
+                common_nodes = list(set(common_nodes).intersection(connection)) 
+                #Remove the common element of the current node and his neigbour
+                connections[node] = list(set(connections[node])- set(common_nodes))
     
     def print_hasse_rec(self, graph, connections, node, parents):
         
@@ -51,11 +74,19 @@ class Hasse:
                 
                 
     def find_connected(self,connections,node):
+        """
+        This function find which nodes are connected a node and 
+        return a list:
+            
+            connections -- Hashmap that contains the node as key and his 
+            connection as value
+            
+            node -- the node that we will find the connections
+        """
         result = []
-        for aNode in connections : 
-            if node in connections[aNode]:
-                result.append(aNode)
+        for a_node in connections : 
+            if node in connections[a_node]:
+                #For all the nodes in connections, look if node is connected
+                result.append(a_node)
         return result       
         
-    def get_connected_node(self,connections,node):
-        return connections[node]
