d46453b44aa124f49c50225945c28ebcb2979dd4
Boubacar Telly Bah <boubacar-telly.bah@polymtl.ca>
2017-11-15 09:44:59 -0500

    Ajout des fichier

---
+++ TP2/src/state.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 15 09:36:14 2017
+
+@author: Boubacar,Leandre,Adber
+"""
+class State : 
+    
+    def  __init__(next_states,is_final):
+        self.next_states = []
+        self.is_final = False
+
+    def add_state(self,state):
+        pass
+       
+        
