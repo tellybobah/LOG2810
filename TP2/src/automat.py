f60f0596629f3c38b64353b008eb4a5a5d18c706
tellybobah <boubacar-telly.bah@polymtl.ca>
2017-11-22 14:19:31 -0500

    Ajout d'entete de fonction

--- TP2/src/automat.py
+++ TP2/src/automat.py
@@ -2,18 +2,26 @@
 """
 Created on Wed Nov 15 09:36:08 2017
 
-@author: Boubacar
+@author: Boubacar,Leandre,Abder
 """
 from state import State
 from district import District
 
 class Automat : 
     
+ 
     def __init__(self):
         self.initial_state = State()
     
     def create_state_adress(self,file):
+        """
+        create_state_adress : methode qui va lire le fichier contenant 
+        les adresses et va creer un automate
+            :param self: 
+            :param file: nom du fichier qui va contenir les adresses
+        """   
         current_node = self.initial_state
+        #Lire le fichier
         with open(file, "r") as f:
             while(True):
                 line = f.readline().strip('\n').strip(' ').strip('\r')
@@ -24,6 +32,7 @@ class Automat :
                 
                 current_node = self.initial_state
                 counter = 0
+                #
                 while not counter == 6:
                     node = line[counter]
                     found_node = False
@@ -31,7 +40,7 @@ class Automat :
                         if state.value == node :
                             current_node = state
                             found_node = True
-
+                    #Si il n'est pas deja dans les fils
                     if  not found_node :
                         inserted_state = None
                         if counter == 5:
@@ -43,6 +52,11 @@ class Automat :
                     counter = counter + 1
 
     def verify_adress(self, adress):
+        """
+        verify_adress : verifie qu'une adresse est bien valide selon l'automate
+            :param self: 
+            :param adress: adresses qui va etre verifiee
+        """   
         counter = 0
         current_node = self.initial_state
         while not counter == 6:
@@ -58,13 +72,22 @@ class Automat :
             
             counter = counter + 1
         return True
-    
+
     def get_adress(self, adress):
+        """
+        get_adress : retourne le noeud final de l'adresse passee en parametre si elle existe
+            :param self: 
+            :param adress: adresse qui va etre verifee
+        """   
         counter = 0
         current_node = self.initial_state
+
+        #Verifie que la taille est bien valide
         while not counter == 6:
             node = adress[counter]
             found_next_state = False
+            
+            #Chercher dans la liste de fils
             for state in current_node.next_states:
                 if state.value == node :
                     current_node = state
