b1a632925212ba00978b873f6c9de747e6901949
tellybobah <boubacar-telly.bah@polymtl.ca>
2017-11-22 09:05:33 -0500

    Ajout des drones et package

---
+++ TP2/src/drone.py
@@ -0,0 +1,6 @@
+class Drone : 
+
+    def __init__(self,curent_position,max_weight,package):
+        self.curent_position = curent_position
+        self.max_weight = max_weight
+        self.package = package
---
+++ TP2/src/package.py
@@ -0,0 +1,5 @@
+class Package : 
+
+    def __init__(self,mass,destination):
+        self.mass  = mass
+        self.destination  = destination
